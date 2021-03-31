3b30ab1b7b0dd9b8c3875e1e9e83379379f6c70f
JedrzejLapawa <lapawajedrzej@gmail.com>
2021-03-31 20:58:42 +0200

    Dodatkowa tablica z czasem

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 setInterval(setClock, 1000)
+setInterval(timeTableClock, 1000)
 
 const hourHand = document.querySelector('[data-hour-hand]')
 const minuteHand = document.querySelector('[data-minute-hand]')
 const secondHand = document.querySelector('[data-second-hand]')
+const timeBoard = document.querySelector(".time__table--span")
 
-function setClock() {
+function timeTableClock() {
   const currentDate = new Date()
-  const secondsRatio = currentDate.getSeconds() / 60
-  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
-  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
-  setRotation(secondHand, secondsRatio)
-  setRotation(minuteHand, minutesRatio)
-  setRotation(hourHand, hoursRatio)
+  const secondsRatio = currentDate.getSeconds() < 10 ? "0" + currentDate.getSeconds() : currentDate.getSeconds() 
+  const minutesRatio = currentDate.getMinutes() < 10 ? "0" + currentDate.getMinutes() : currentDate.getMinutes()
+  const hoursRatio = currentDate.getHours() < 10 ? "0" + currentDate.getHours() : currentDate.getHours()
+   
+  timeBoard.innerHTML = `${hoursRatio} : ${minutesRatio} : ${secondsRatio}`
+}
+
+function setClock() {  
+    const currentDate = new Date()
+    const secondsRatio = currentDate.getSeconds() / 60
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+    setRotation(secondHand, secondsRatio)
+    setRotation(minuteHand, minutesRatio)
+    setRotation(hourHand, hoursRatio)
 }
 
 function setRotation(element, rotationRatio) {
@@ -19,3 +30,4 @@ function setRotation(element, rotationRatio) {
 }
 
 setClock()
+timeTableClock()
